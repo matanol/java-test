c8210e66c200abb23f0921956a9c8789982202f8
matanm <matanoliel20@gmail.com>
2023-03-09 19:33:29 +0200

    added try and catch

--- Main.java
+++ Main.java
@@ -1,24 +1,35 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter Toyota max speed:");
+        try {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter Toyota max speed:");
+            int toyotaMaxSpeedInput = input.nextInt();
 
-        String toyotaMaxSpeedInput = input.nextLine();
+            System.out.println("Enter Mazda max speed:");
+            int mazdaMaxSpeedInput = input.nextInt();
 
-        System.out.println("Enter Mazda max speed:");
-        String mazdaMaxSpeedInput = input.nextLine();
+            Car toyota = new Car("Toyota Corolla", toyotaMaxSpeedInput);
+            Car mazda = new Car("Mazda 6", mazdaMaxSpeedInput);
 
+            if (toyota._maxSpeed > mazda._maxSpeed) {
+                System.out.print(toyota._name + " is faster");
+            } else {
+                System.out.print(mazda._name + " is faster");
+            }
+        } catch (Exception e) {
+            if (e instanceof InputMismatchException) {
+                System.out.println("Wrong input type");
+                main(new String[0]);
 
-        Car toyota = new Car("Toyota Corolla", Integer.parseInt(toyotaMaxSpeedInput));
-        Car mazda = new Car("Mazda 6", Integer.parseInt(mazdaMaxSpeedInput));
-
-        if (toyota._maxSpeed > mazda._maxSpeed) {
-            System.out.print(toyota._name + " is faster");
-        } else {
-            System.out.print(mazda._name + " is faster");
+            } else {
+                System.out.println(e);
+                System.exit(1);
+            }
         }
+
     }
 }
 
